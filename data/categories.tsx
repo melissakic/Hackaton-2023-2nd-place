bbba2d00f3eb5b3d20d94155bf92eddd7a7efca4
Ivana <ivana.pavic@fet.ba>
2023-03-24 18:30:40 +0100

    Start2

---
+++ data/categories.tsx
@@ -0,0 +1,13 @@
+const categories= [
+
+    {id: '1', name: 't-shirt', image: require('../categories/tshirt.png')},
+    {id: '2', name: 'jeans', image: require('../categories/jeans.png')},
+    {id: '3', name: 'jacket', image: require('../categories/jacket.png')},
+    {id: '4', name: 'shoes', image: require('../categories/shoes.png')},
+
+
+
+
+];
+
+export default categories;
