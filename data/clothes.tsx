2cf4d35e394a2d73ca16a27f244833d661cff91f
Ivana <ivana.pavic@fet.ba>
2023-03-25 02:01:39 +0100

    Pocetna i artikli-dio

---
+++ data/clothes.tsx
@@ -0,0 +1,32 @@
+const clothes = [
+    {
+        id: '1',
+        name: 'Ženska košulja',
+        ingredients: 'Mixed Pizza',
+        price: '8.30',
+        image: require('../assets/kosulja.png'),
+    },
+    {
+        id: '2',
+        name: 'Ženska jakna',
+        ingredients: 'Cheese Pizza',
+        price: '7.10',
+        image: require('../assets/jakna.png'),
+    },
+    {
+        id: '3',
+        name: 'Patike',
+        ingredients: 'Fried Chicken',
+        price: '5.10',
+        image: require('../assets/patike.png'),
+    },
+    {
+        id: '4',
+        name: 'Muške hlače',
+        ingredients: 'Salmon Meat',
+        price: '9.55',
+        image: require('../assets/hlace.png'),
+    },
+];
+
+export default clothes;
